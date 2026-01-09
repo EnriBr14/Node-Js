ba0443066e23d7ec2f671ca445da448df143dff0
EnGar <garcianairb14@gmail.com>
2026-01-08 20:56:04 -0600

    Manejo de paquetes

---
+++ src/FormatoFechas/Index.jx.js
@@ -0,0 +1,5 @@
+function date(){
+    return Date.now()
+}
+
+console.log(typeof Date)
---
+++ src/NPM/main.js
@@ -0,0 +1,6 @@
+//Instalar el paquete isNumber del sitio de npm
+// url "https://www.npmjs.com/package/isnumber"
+//Instalador "npm i isnumber"
+let isNumber = require('isnumber')
+console.log("Es un numero: "+isNumber(45));
+console.log(isNumber('Hla'))
